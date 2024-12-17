79427ecb9f64fdf41cdcb82a0a2c59ed0f45b127
Francisco Torres Pérez <franute@gmail.com>
2024-12-17 15:19:31 +0100

    Improve disableRepos with wildcard, add devPod to dev images

--- files/scripts/dev/disableRepos.sh
+++ files/scripts/dev/disableRepos.sh
@@ -4,6 +4,7 @@ set -eoux pipefail
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-lazygit-fedora-41.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-starship-fedora-41.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-ubuntu-fonts-fedora-41.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-lazygit-*.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-starship-*.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-ubuntu-fonts-*.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ublue-os-staging-*.repo
--- files/scripts/home/disableRepos.sh
+++ files/scripts/home/disableRepos.sh
@@ -4,4 +4,4 @@ set -eoux pipefail
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-ubuntu-fonts-fedora-41.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-ubuntu-fonts-*.repo
