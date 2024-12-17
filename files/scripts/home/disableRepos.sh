d96c5fe5962c175973059e72036940070a559964
Francisco Torres Pérez <franute@gmail.com>
2024-12-17 13:15:11 +0100

    Disable added repos before signing image

---
+++ files/scripts/home/disableRepos.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+set -eoux pipefail
+
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
+
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-ubuntu-fonts-fedora-41.repo
