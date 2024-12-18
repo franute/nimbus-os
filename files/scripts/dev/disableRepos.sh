7eae7a9622d8a7686442ea0a8a1bc11e8a01fcbf
Francisco Torres Pérez <franute@gmail.com>
2024-12-18 17:07:17 +0100

    Do not install devpod by default and add podman-compose and podman-docker

--- files/scripts/dev/disableRepos.sh
+++ files/scripts/dev/disableRepos.sh
@@ -7,4 +7,3 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-lazygit-*.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-starship-*.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-ubuntu-fonts-*.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ublue-os-staging-*.repo
