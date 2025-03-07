b094303c6fa678925ec5da56322df8639aef701b
Francisco Torres Pérez <franute@gmail.com>
2025-03-07 19:56:23 +0100

    Adding ublue repos and gnome-shell extension

--- files/scripts/dev/disableRepos.sh
+++ files/scripts/dev/disableRepos.sh
@@ -7,3 +7,4 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-lazygit-*.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-starship-*.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-ubuntu-fonts-*.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:ublue-os:staging.repo
