0999fd9d1cbd8d09dc0ea766b47be2cc72846a9f
Francisco Torres Pérez <franute@gmail.com>
2025-04-24 15:31:31 +0200

    More updates to the new dnf5 module: - adding google chrome repo - import google chrome key - disable all repos with the new module - remove fira fonts - remove defaulting ubuntu fonts

--- files/scripts/common/disableRepos.sh
+++ files/scripts/common/disableRepos.sh
@@ -3,6 +3,3 @@
 set -eoux pipefail
 
 dnf config-manager setopt fedora-cisco-openh264.enabled=0
-dnf config-manager setopt google-chrome.enabled=0
-
-dnf config-manager setopt copr:copr.fedorainfracloud.org:atim:ubuntu-fonts.enabled=0
