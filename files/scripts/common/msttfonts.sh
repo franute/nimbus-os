f91c9c78d9830c274e573e72959b239a32bff1e4
Francisco Torres Pérez <franute@gmail.com>
2025-12-17 13:47:24 +0100

    fix: fix mscorefonts installer

--- files/scripts/common/msttfonts.sh
+++ files/scripts/common/msttfonts.sh
@@ -8,10 +8,5 @@ set -oue pipefail
 # Your code goes here.
 echo "Installing dependencies"
 dnf install -y cabextract xorg-x11-font-utils
-echo "Downloading mscorefonts2 installer"
-curl https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm -o /tmp/msttcore-fonts-installer-2.6-1.noarch.rpm
-echo "Installing mscorefonts2 installer"
-rpm -i --nodigest /tmp/msttcore-fonts-installer-2.6-1.noarch.rpm
-
-echo "Deleting downloaded files"
-rm /tmp/msttcore-fonts-installer-2.6-1.noarch.rpm
+echo "Installing mscorefonts2"
+rpm -i --nodigest https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
